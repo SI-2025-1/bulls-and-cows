c927b764c671469aa5233dbda5cbfaeadafadb4d
Jhonatan Torres <jhonatan.torres.247@outlook.com>
2025-04-16 23:16:44 -0500

    fix: tests

--- tests/agents/test_codebreaker_agent.py
+++ tests/agents/test_codebreaker_agent.py
@@ -19,34 +19,36 @@ class TestCodeBreakerAgent:
         agent = CodeBreakerAgent()
         assert agent.possible_numbers is not None
         assert agent.possible_numbers == self.possible_numbers
-        assert agent.guess is None
+        assert agent.last_guess is self.possible_numbers[0]
 
         generate_all_4_number_permutations_mock.assert_called_once()
 
-    def test_get_latest_guess(self, generate_all_4_number_permutations_mock):
+    def test_get_next_guess(self, generate_all_4_number_permutations_mock):
         agent = CodeBreakerAgent()
 
-        guess = agent._get_latest_guess()
+        guess = agent._get_next_guess()
 
         assert guess == self.possible_numbers[0]
-        assert agent.guess == self.possible_numbers[0]
+        assert agent.last_guess == self.possible_numbers[0]
 
         agent.possible_numbers = []
 
-        with pytest.raises(Exception, match="Error in perception: no possible numbers"):
-            agent._get_latest_guess()
+        with pytest.raises(
+            Exception, match="There are no more possible guesses based on the feedback"
+        ):
+            agent._get_next_guess()
 
-    def test_process_feedback(self, generate_all_4_number_permutations_mock):
+    def test_calculate_next_guess(self, generate_all_4_number_permutations_mock):
         agent = CodeBreakerAgent()
-        agent.guess = "1234"
+        agent.last_guess = "1234"
 
-        agent._process_feedback(1, 1)
+        agent._calculate_next_guess(1, 1)
 
         assert agent.possible_numbers == ["9201"]
 
         agent.possible_numbers = self.possible_numbers.copy()
 
-        agent.guess = "5678"
-        agent._process_feedback(0, 0)
+        agent.last_guess = "5678"
+        agent._calculate_next_guess(0, 0)
 
         assert agent.possible_numbers == ["1234", "9201"]
